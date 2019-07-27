80521840ac419090a0cd15e6996b61af796abf7d
Peter Czimmermann <peter.czimmermann@sensorup.com>
2019-07-27 10:57:18 -0700

    remove resizable

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import './App.css';
 import ReactWordcloud from 'react-wordcloud';
-import {Resizable} from 're-resizable';
 import words from './words';
-
-const resizeStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  border: 'solid 1px #ddd',
-  background: '#f0f0f0',
-};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        SMS World Cloud
+        SMS Word Cloud
       </header>
       <div className="App-body">
-        <Resizable
-            defaultSize={{
-              width: '100%',
-              height: '100%',
-            }}
-            style={resizeStyle}>
           <div style={{width: '100%', height: '100%'}}>
             <ReactWordcloud words={words} />
           </div>
-        </Resizable>
       </div>
     </div>
   );
