fef5a0c75cec688ecbe48fed62cdcf1a6cb39d73
Peter Czimmermann <peter.czimmermann@sensorup.com>
2019-07-27 14:02:18 -0700

    working poc

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,64 @@
 import React from 'react';
 import './App.css';
 import ReactWordcloud from 'react-wordcloud';
-import words from './words';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        SMS Word Cloud
-      </header>
-      <div className="App-body">
-          <div style={{width: '100%', height: '100%'}}>
-            <ReactWordcloud words={words} />
-          </div>
-      </div>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            words: []
+        };
+    }
+
+    loadWords() {
+        fetch("https://l1aycdz6w6.execute-api.us-west-1.amazonaws.com/dev/words")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        words: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    componentDidMount() {
+        this.loadWords();
+    }
+
+    render() {
+        const {error, isLoaded, words} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        SMS Word Cloud
+                    </header>
+                    <div className="App-body">
+                        <div style={{width: '100%', height: '100%'}}>
+                            <ReactWordcloud words={words}/>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
--- handler.py
+++ handler.py
@@ -6,18 +6,80 @@ import urllib.parse
 import twilio.twiml
 
 kms = boto3.client('kms')
+dynamodb = boto3.client('dynamodb')
 
 SECRETS = json.loads(kms.decrypt(
     CiphertextBlob=base64.b64decode(os.environ['SECRETS'])
 )['Plaintext'].decode("utf-8"))
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+
+def update_counter(word, book="DEFAULT"):
+    response = dynamodb.update_item(
+        TableName='words',
+        Key={
+            'Book': {'S': book},
+            'BookWord': {'S': word}
+        },
+        UpdateExpression='SET wordCount = if_not_exists(wordCount, :init) + :inc',
+        ExpressionAttributeValues={
+            ':inc': {'N': '1'},
+            ':init': {'N': '0'},
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    print("INC word " + word + " in " + book)
+    print(response)
+
+
+def words_static(event, context):
+    with open('words.json', 'r') as myfile:
+        words = json.load(myfile)
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*'
+        },
+        "body": json.dumps(words)
     }
+
+def words(event, context):
+    resp = dynamodb.query(
+        TableName='words',
+        KeyConditionExpression="Book = :book",
+        ExpressionAttributeValues={
+            ':book': {
+                'S': 'DEFAULT',
+            },
+        }
+    )
+    print(resp)
+    wc = [{'text':item['BookWord']['S'],'value':item['wordCount']['N']} for item in resp['Items']]
+    # with open('words.json', 'r') as myfile:
+    #     words = json.load(myfile)
+    #     for word in words:
+    #         wc.append(word)
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*'
+        },
+        "body": json.dumps(wc)
+    }
+
+
+def sms(event, context):
     print(json.dumps(event))
-    print(json.dumps(urllib.parse.parse_qs(event['body'])))
+    # GET requests
+    if event['queryStringParameters'] is not None:
+        for word in event['queryStringParameters']['Body'].split():
+            if len(word) > 1:
+                update_counter(word)
+    # POST requests
+    if 'Body' in urllib.parse.parse_qs(event['body']):
+        for words in urllib.parse.parse_qs(event['body'])['Body']:
+            for word in words.split():
+                if len(word) > 1:
+                    update_counter(word)
 
     response = {
         "statusCode": 200,
